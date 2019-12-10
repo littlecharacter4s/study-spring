4e101a36afd4ca6a3b37a1ad9994a3afc2d93a4d
littlecharacter <littlecharacter@qq.com>
2019-12-11 03:11:48 +0800

    Spring MVC demo

--- study-spring-mvc/src/main/java/com/lc/spring/mvc/config/AppConfig.java
+++ study-spring-mvc/src/main/java/com/lc/spring/mvc/config/AppConfig.java
@@ -1,16 +1,19 @@
 package com.lc.spring.mvc.config;
 
-import com.lc.spring.mvc.interceptor.GlobalInterceptor;
-import com.lc.spring.mvc.interceptor.SpecialInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
@@ -19,43 +22,39 @@ import java.io.IOException;
         useDefaultFilters = false
 )
 public class AppConfig implements WebMvcConfigurer {
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        // registry.jsp("/views/", ".html");
-        registry.freeMarker().prefix("/views/").suffix(".html").cache(false);;
-    }
-
     @Bean
     public FreeMarkerConfigurer freeMarkerConfigurer() throws IOException {
         // Configure FreeMarker...
         FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
 
-        // configurer.setTemplateLoaderPath("/views/");
-        // Properties properties = new Properties();
-        // properties.setProperty("template_update_delay", "0");
-        // properties.setProperty("default_encoding", "UTF-8");
-        // properties.setProperty("number_format", "0.##########");
-        // properties.setProperty("datetime_format", "yyyy-MM-dd HH:mm:ss");
-        // properties.setProperty("classic_compatible", "true");
-        // properties.setProperty("template_exception_handler", "ignore");
-        // configurer.setFreemarkerSettings(properties);
+        configurer.setTemplateLoaderPath("/views/");
+        Properties properties = new Properties();
+        properties.setProperty("template_update_delay", "0");
+        properties.setProperty("default_encoding", "UTF-8");
+        properties.setProperty("number_format", "0.##");
+        properties.setProperty("date_format", "yyyy-MM-dd");
+        properties.setProperty("time_format", "HH:mm:ss");
+        properties.setProperty("datetime_format", "yyyy-MM-dd HH:mm:ss");
+        properties.setProperty("classic_compatible", "true");
+        properties.setProperty("template_exception_handler", "ignore");
+        configurer.setFreemarkerSettings(properties);
 
         return configurer;
     }
-//
-//     @Bean
-//     public FreeMarkerViewResolver freeMarkerViewResolver() {
-//         FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
-//         viewResolver.setSuffix(".html");
-//         viewResolver.setContentType("text/html;charset=UTF-8");
-//         return viewResolver;
-//     }
+
+    @Bean
+    public FreeMarkerViewResolver freeMarkerViewResolver() {
+        // Configure ViewResolver...
+        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+        viewResolver.setSuffix(".html");
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        return viewResolver;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new GlobalInterceptor()).addPathPatterns("/**");
-        registry.addInterceptor(new SpecialInterceptor()).addPathPatterns("/springmvc/**").excludePathPatterns("/springmvc/test/**");
+        // registry.addInterceptor(new GlobalInterceptor()).addPathPatterns("/**");
+        // registry.addInterceptor(new SpecialInterceptor()).addPathPatterns("/springmvc/**").excludePathPatterns("/springmvc/test/**");
     }
 
     //静态资源访问
