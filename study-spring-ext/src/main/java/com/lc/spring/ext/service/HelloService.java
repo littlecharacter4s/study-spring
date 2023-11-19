d642f8d8c84208c565688efdb940f482129cbcf8
gujixian <littlecharacter@qq.com>
2023-11-20 01:07:20 +0800

    HelloService

---
+++ study-spring-ext/src/main/java/com/lc/spring/ext/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.lc.spring.ext.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author gujixian
+ * @since 2023/11/20
+ */
+@Component
+public class HelloService {
+}
